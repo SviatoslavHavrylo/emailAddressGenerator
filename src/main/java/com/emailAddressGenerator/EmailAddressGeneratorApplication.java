764f8f8173e3af5c0eea5a927731e0a2d5b09192
SviatoslavHavrylo <s.gavrilo@infinitytechnologies.ch>
2021-05-06 15:20:33 +0300

    logger

--- src/main/java/com/emailAddressGenerator/EmailAddressGeneratorApplication.java
+++ src/main/java/com/emailAddressGenerator/EmailAddressGeneratorApplication.java
@@ -1,9 +1,11 @@
 package com.emailAddressGenerator;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class EmailAddressGeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmailAddressGeneratorApplication.class, args);
